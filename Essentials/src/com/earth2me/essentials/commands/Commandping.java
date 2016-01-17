de1e36d5e778ba1b852477f814aba3b79a9d4a52
HunterGPlays <huntergplays@gmail.com>
2016-01-17 23:34:51 +0000

    Make ping more useful (REFLECTION)
    This *should* work.

--- Essentials/src/com/earth2me/essentials/commands/Commandping.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandping.java
@@ -1,8 +1,14 @@
 package com.earth2me.essentials.commands;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import com.earth2me.essentials.CommandSource;
-import com.earth2me.essentials.utils.FormatUtil;
+import com.earth2me.essentials.User;
+
+import org.bukkit.ChatColor;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 
 import static com.earth2me.essentials.I18n.tl;
 
@@ -15,10 +21,44 @@ public class Commandping extends EssentialsCommand {
     @Override
     public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
         if (args.length < 1) {
-
-            sender.sendMessage(tl("pong"));
+            if (!sender.isPlayer()) {
+                sender.sendMessage("Pong!");
+                return;
+            }
+            int ping = getPing(sender.getPlayer(), server);
+            sender.sendMessage(tl("ping", "" + getPingColor(ping) + ping));
         } else {
-            sender.sendMessage(FormatUtil.replaceFormat(getFinalArg(args, 0)));
+            final User user = getPlayer(server, args, 0, true, true);;
+            //if (!player.isOnline()) {
+            //    sender.sendMessage(tl("errorWithMessage", player + "is not online."));
+            //    return;
+            //}
+
+            int ping = getPing((Player) user, server);
+            sender.sendMessage(tl("pingOfPlayer", user.getDisplayName(), "" + getPingColor(ping) + ping));
         }
+    }
+
+    public int getPing(final Player player, final Server server) throws Exception {
+        // REFLECTION
+        final String serverVersion = server.getClass().getPackage().getName().split("\\.")[3];
+        final Class<?> craftPlayer = Class.forName("org.bukkit.craftbukkit."+serverVersion+".entity.CraftPlayer");
+        final Object converted = craftPlayer.cast(player);
+        final Method handle = converted.getClass().getMethod("getHandle");
+        final Object entityPlayer = handle.invoke(converted);
+        Field pingField = entityPlayer.getClass().getField("ping");
+        return pingField.getInt(entityPlayer);
+    }
+
+    public ChatColor getPingColor(int ping) {
+        ChatColor color;
+        if (ping <= 200) {
+            color = ChatColor.GREEN;
+        } else if (ping <= 500) {
+            color = ChatColor.YELLOW;
+        } else {
+            color = ChatColor.RED;
+        }
+        return color;
     }
 }
